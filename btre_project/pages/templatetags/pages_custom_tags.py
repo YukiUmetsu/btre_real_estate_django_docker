1f8182e62e5fe51ff2687774e4a7903c01ea6802
YukiUmetsu <yuuki.umetsu@gmail.com>
2020-02-27 11:18:36 -0600

    added search functionality

--- btre_project/pages/templatetags/pages_custom_tags.py
+++ btre_project/pages/templatetags/pages_custom_tags.py
@@ -8,3 +8,7 @@ def is_active(context, target_path):
         return 'active' if target_path == context['request'].path else ''
 
     return 'active' if target_path in context['request'].path else ''
+
+@register.simple_tag()
+def print_if_equal(target1, target2, print_value='selected'):
+    return print_value if target1 == target2 else ''
