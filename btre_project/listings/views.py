52e97b0c24ad28991f4a7e4c14651d48d1551133
YukiUmetsu <yuuki.umetsu@gmail.com>
2020-02-26 22:54:56 -0600

    added dynamic content in pages index, about, listings listing

---
+++ btre_project/listings/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+    page_number = request.GET.get('page')
+    paged_listings = paginator.get_page(page_number)
+    context = {'listings': paged_listings}
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {'listing': listing}
+    return render(request, 'listings/listing.html', context)
+
+def search(request):
+    return render(request, 'listings/search.html')
--- btre_project/pages/templatetags/pages_custom_tags.py
+++ btre_project/pages/templatetags/pages_custom_tags.py
@@ -4,4 +4,7 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def is_active(context, target_path):
-    return 'active' if context['request'].path == target_path else ''
+    if target_path == '/':
+        return 'active' if target_path == context['request'].path else ''
+
+    return 'active' if target_path in context['request'].path else ''
