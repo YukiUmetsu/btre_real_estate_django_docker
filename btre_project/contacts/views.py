48049a1122ae3208a8418a2004b9bbe1db40af4b
YukiUmetsu <yuuki.umetsu@gmail.com>
2020-02-27 21:37:01 -0600

    added dashboard functionality

--- btre_project/contacts/views.py
+++ btre_project/contacts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Contact
+from django.core.mail import send_mail
+import os
 
 def contact(request):
     if request.method != 'POST':
@@ -17,6 +19,14 @@ def contact(request):
 
     contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
     contact.save()
+
+    send_mail(
+        'Inquary for '+listing,
+        message,
+        os.environ.get("EMAIL_HOST_USER"),
+        [realtor_email],
+        fail_silently=False,
+    )
     messages.success(request, 'Your request has been submitted!')
 
     return redirect('/listings/'+listing_id)
